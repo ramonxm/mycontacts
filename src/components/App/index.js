8abad34e7875c31ef59f1d990a84cf72a9fffe38
Ramon <ramon1234rxm@gmail.com>
2021-11-16 09:32:45 -0300

    feat: stylize app and move to components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,22 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import theme from '../../styles/theme';
 import GlobalStyle from '../../styles/global';
 
 import { Container } from './style';
+import { Header } from '../Header';
+import { ContactsList } from '../ContactList';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Container>Olá, mundo!</Container>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Container>
+          <Header />
+          <ContactsList />
+        </Container>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
