b39f7130847e825519a5b904842e348c6ae3c805
Ramon <ramon1234rxm@gmail.com>
2021-11-16 09:33:53 -0300

    style: update theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,15 @@
 export default {
   palette: {
     backgroundColor: '#f6f5fc',
+    primary: {
+      lighter: '#e0e3ff',
+      light: '#6674f4',
+      main: '#5061fc',
+      dark: '#3346f0',
+    },
+    gray: {
+      900: '#222',
+      200: '#bcbcbc',
+    },
   },
 };
