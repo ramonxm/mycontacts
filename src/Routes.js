d6f85a94d17ab4bdcf29146e08cc82144507b283
Ramon <ramon1234rxm@gmail.com>
2021-11-22 10:04:11 -0300

    chore: make a routes

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import { Routes as Switch, Route } from 'react-router-dom';
+
+import { EditContact, Home, NewContact } from './pages';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<NewContact />} />
+      <Route path="/edit/:id" element={<EditContact />} />
+    </Switch>
+  );
+}
