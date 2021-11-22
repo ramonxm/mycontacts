b17b6134c4bdbad645469fd71cf16baea617c97d
Ramon <ramon1234rxm@gmail.com>
2021-11-22 10:03:17 -0300

    feat: make and stylize page header

---
+++ src/components/PageHeader/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  margin-bottom: 24px;
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    span {
+      font-weight: bold;
+      color: ${({ theme }) => theme.palette.primary.main};
+    }
+    img {
+      margin-left: 8px;
+      transform: rotate(-90deg);
+    }
+  }
+`;
