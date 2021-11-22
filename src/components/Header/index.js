a63b34ed22ba1578cb0ded8314334367462e43bf
Ramon <ramon1234rxm@gmail.com>
2021-11-22 10:03:03 -0300

    feat: creating a header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,7 @@
-import { ContainerHeader, InputSearchContainer } from './style';
+import { ContainerHeader } from './style';
 
 export const Header = () => (
   <ContainerHeader>
     <img src="/assets/svg/logo.svg" alt="MyContacts" />
-    <InputSearchContainer>
-      <input type="text" placeholder="Pesquisar pelo nome..." />
-    </InputSearchContainer>
   </ContainerHeader>
 );
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,32 +1,12 @@
 import styled from 'styled-components';
 
 export const ContainerHeader = styled.header`
-  margin-top: 74px;
   display: flex;
+  margin: 74px 0 48px;
   flex-direction: column;
   align-items: center;
 
   img {
     width: 201px;
-  }
-`;
-
-export const InputSearchContainer = styled.div`
-  margin-top: 48px;
-  width: 100%;
-
-  input {
-    width: 100%;
-    background: #fff;
-    border: none;
-    border-radius: 25px;
-    height: 50px;
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
-    outline: 0;
-    padding: 0 16px;
-
-    &::placeholder {
-      color: #bcbcbc;
-    }
   }
 `;
