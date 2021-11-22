79be43ce3bf68d562138f048961635b71653867d
Ramon <ramon1234rxm@gmail.com>
2021-11-22 10:03:53 -0300

    feat: make a select component

---
+++ src/components/Select.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Select = styled.select`
+  background: #fff;
+  width: 100%;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border: 2px solid #fff;
+  height: 52px;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+  transition: border-color .2s ease-in;
+
+  &:focus{
+    border-color: ${({ theme }) => theme.palette.primary.main};
+  }
+`;
