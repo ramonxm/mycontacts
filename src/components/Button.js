d4094f2ebb7503d158d04478aaa6dbdf46de13b5
Ramon <ramon1234rxm@gmail.com>
2021-11-22 10:50:24 -0300

    refactor: prop width in button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  width: 100%;
   height: 52px;
+  padding: 0 16px;
   border: none;
   background: ${({ theme }) => theme.palette.primary.main};
   font-size: 16px;
