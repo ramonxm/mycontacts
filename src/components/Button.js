d804e426f12d493e633e240d01b94e7a32df9d17
Ramon <ramon1234rxm@gmail.com>
2021-11-22 10:03:28 -0300

    feat: make a button component

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  width: 100%;
+  height: 52px;
+  border: none;
+  background: ${({ theme }) => theme.palette.primary.main};
+  font-size: 16px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  font-weight: bold;
+  color: #fff;
+  border-radius: 4px;
+  transition: background .2 ease-in;
+
+  &:hover {
+    background: ${({ theme }) => theme.palette.primary.ligth};
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.palette.primary.dark};
+  }
+
+  &[disabled] {
+    background: #ccc;
+    cursor: default;
+  }
+`;
