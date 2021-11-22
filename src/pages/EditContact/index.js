e9e7f27c3d8941bd8e8f3c09e768298e963aa1b4
Ramon <ramon1234rxm@gmail.com>
2021-11-22 10:04:26 -0300

    feat: create a edit contact page

---
+++ src/pages/EditContact/index.js
@@ -0,0 +1,9 @@
+import { PageHeader } from '../../components/PageHeader';
+import { ContactForm } from '../../components/ContactForm';
+
+export const EditContact = () => (
+  <>
+    <PageHeader title="Editar Ramon Xavier" />
+    <ContactForm buttonLabel="Salvar alterações" />
+  </>
+);
