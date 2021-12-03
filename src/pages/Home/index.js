fbeafc165ff535233e37ce9a8f98ee921b893d8a
Ramon <ramon1234rxm@gmail.com>
2021-12-03 09:30:31 -0300

    feat: make a request to list contacts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,42 +1,75 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Container, InputSearchContainer, Header, ListContainer, Card,
+  Container, InputSearchContainer, Header, ListHeader, Card,
 } from './style';
 
-export const Home = () => (
-  <Container>
-    <InputSearchContainer>
-      <input type="text" placeholder="Pesquisar pelo nome..." />
-    </InputSearchContainer>
-    <Header>
-      <strong>3 contatos</strong>
-      <Link to="/new-contact">Novo contato</Link>
-    </Header>
-    <ListContainer>
-      <header>
-        <button type="button">
+const baseURL = 'http://localhost:3001';
+
+export const Home = () => {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+
+  useEffect(() => {
+    fetch(`${baseURL}/contacts?orderBy=${orderBy}`)
+      .then(async (response) => {
+        const contact = await response.json();
+        setContacts(contact);
+      }).catch((error) => {
+        throw new Error(error);
+      });
+  }, [orderBy]);
+
+  const handleToggleOrderBy = () => {
+    setOrderBy((prevState) => (
+      prevState === 'asc' ? 'desc' : 'asc'
+    ));
+  };
+
+  return (
+    <Container>
+
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar pelo nome..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' contato' : ' contatos'}
+        </strong>
+        <Link to="/new-contact">Novo contato</Link>
+      </Header>
+
+      <ListHeader orderBy={orderBy}>
+        <button type="button" onClick={handleToggleOrderBy}>
           <span>Nome</span>
           <img src="/assets/svg/arrow.svg" alt="arrow" />
         </button>
-      </header>
-      <Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong>Ramon Xavier</strong>
-            <small>instagram</small>
+      </ListHeader>
+
+      {contacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              {contact.category_name && (
+              <small>{contact.category_name}</small>
+              )}
+            </div>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
           </div>
-          <span>ramon1234rxm@gmail.com</span>
-          <span>(21) 98929-9455</span>
-        </div>
-        <div className="actions">
-          <a href="/">
-            <img src="/assets/svg/edit.svg" alt="Edit" />
-          </a>
-          <button type="button">
-            <img src="/assets/svg/trash.svg" alt="Trash" />
-          </button>
-        </div>
-      </Card>
-    </ListContainer>
-  </Container>
-);
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src="/assets/svg/edit.svg" alt="Edit" />
+            </Link>
+            <button type="button">
+              <img src="/assets/svg/trash.svg" alt="Trash" />
+            </button>
+          </div>
+        </Card>
+      ))}
+    </Container>
+  );
+};
