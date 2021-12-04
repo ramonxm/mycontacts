237098f738a1081a941fafbc26bde8207e89576a
Ramon <ramon1234rxm@gmail.com>
2021-12-04 20:14:38 -0300

    chore: added isloading in loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,9 +1,19 @@
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import { Overlay } from './style';
 
-export const Loader = () => ReactDOM.createPortal(
-  <Overlay>
-    <div className="loader" />
-  </Overlay>,
-  document.getElementById('loader-root'),
-);
+export const Loader = ({ isLoading }) => {
+  if (!isLoading) {
+    return null;
+  }
+  return ReactDOM.createPortal(
+    <Overlay>
+      <div className="loader" />
+    </Overlay>,
+    document.getElementById('loader-root'),
+  );
+};
+
+Loader.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+};
